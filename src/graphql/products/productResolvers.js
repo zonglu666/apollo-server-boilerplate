9bc3f8354835e4896ea74d4a7d2209f56c2803a4
zonglu666 <415390588@qq.com>
2018-10-27 05:50:55 +0800

    Message

--- src/graphql/products/productResolvers.js
+++ src/graphql/products/productResolvers.js
@@ -4,8 +4,14 @@ import type { ProductType, ProductConnection } from './ProductTypes';
 
 const resolvers: Object = {
   products: async ProductConnection => {
-    const products = ProductModel.find();
-
+    const products = ProductModel.find().populate('brand');
+    products.forEach(function(n){
+        switch(n.status) {
+            case 1: n.status = "正常"; break;
+            case 2: n.status = "不正常"; break;
+            default: n.status = "未知"; break;
+        }
+    })
     return {
       products,
     };
